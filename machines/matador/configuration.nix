3999de4b190401d8ecce4e18e7d003a31587103b
nanashi0x74 <rian.lindenberger@gmail.com>
2020-05-26 19:30:03 +0200

    extracted ssh decrypting as component

--- machines/matador/configuration.nix
+++ machines/matador/configuration.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./components/ssh-decrypt.nix
     ];
 
   # Use the GRUB 2 boot loader.
@@ -28,16 +29,6 @@
   device = "/dev/disk/by-uuid/41dc39e2-5122-4b47-9079-cddcc299af58";
   preLVM = true;
       }];
-      network.enable = true;
-      network.ssh = {
-        enable = true;
-        authorizedKeys = [
-          "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCslUchy7sMNYNUg9a//cgl1pG1HPoPso62VriPtmDDZ956Q9AEVMDK8FU2KVb1ZN1VONdjt4zelQyb4l/cUfoS5HzEc/qry7Q538QgeK7LECW1dXbOioxUL7WcpVR07RJVofoD2mNlyyOGB62wusX9SYWUqDFgjWZj0g2fUzR2lsklRsEEM8ZFGLytmYpJ0FIeOSidn8XM+qHErTTTRrLjEDGTgkmtUFaTVzRobiGnkr5SyDx8uUWqagotoct62HOLb2n01JmJvLqCFbrBdUBgC+jSSwgygqZTGWPD5mxaH9tvBLhNt+7apNc19fNCio8JoxVUyqsBqa12hKeZPK6E4eoUPpK6HWk/LtGRUKYeO01aRa3uqK+VAKHSECss+TuVfcnhKbi8u+AJ9PnrVur7+tC9xlqAf+J154sVGZy6GPYD5zxtskTBqdB93BBko16eB9iKxgPw7uu/4tU1pY1ypRyxzkk4tjLlgX5/Wz0fOv/PXT9LnBYLUD/V2PdUmmc= rian@rian-ThinkPad-S3-Yoga-14
-"];
-        hostDSSKey = /etc/dropbear/dropbear_dss_host_key;
-        hostECDSAKey = /etc/dropbear/dropbear_ecdsa_host_key;
-        hostRSAKey = /etc/dropbear/dropbear_rsa_host_key;
-        port = 7474;
       };
     };
   };
