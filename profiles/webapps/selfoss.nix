fdf11c892957acdca422ec36cbd0aa8c25359318
nanashi0x74 <rian.lindenberger@gmail.com>
2021-07-04 21:26:54 +0200

    add index

--- profiles/webapps/selfoss.nix
+++ profiles/webapps/selfoss.nix
@@ -12,7 +12,8 @@
     nginx = {
       enable = true;
       virtualHosts."nanashi0x74.dev".locations."/feedreader" = {
-        root = /var/lib/selfoss;
+        root = "/var/lib/selfoss";
+        index = "index.php";
         extraConfig = let fpm = config.services.phpfpm.pools.${config.services.selfoss.pool}; in ''
           fastcgi_split_path_info ^(.+\.php)(feedreader/.+)$;
           fastcgi_pass unix:${fpm.socket};
