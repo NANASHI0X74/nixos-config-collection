49fb632ae292ad64e43258bf49d19643e2b85971
nanashi0x74 <rian.lindenberger@gmail.com>
2024-01-29 12:36:46 +0100

    add syncthing package and firewall rules

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -10,11 +10,17 @@ in
     enable = mkBoolOpt false;
   };
   config = mkIf cfg.enable {
+    environment.systemPackages = [ pkgs.syncthing ];
     services.syncthing = {
       enable = true;
       user = "${config.user.name}";
       dataDir = "${config.user.home}/Documents";
       configDir = "${config.user.home}/Documents/.config/syncthing";
     };
+    networking.firewall.allowedTCPPorts = [ 22000 ]; # tcp sync traffic
+    networking.firewall.allowedUDPPorts = [
+      22000 # udp sync traffic
+      21027 # discovery broadcasts
+    ];
   };
 }
